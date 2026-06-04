0ddbccec997830510321afa60eff4b575dec1c82
kellie <kellie@wackywizards.org>
2026-06-04 14:41:00 +0300

    Better summary on GameObject.Spawn extension

--- Code/Extensions/GameObjectExtensions.cs
+++ Code/Extensions/GameObjectExtensions.cs
@@ -5,9 +5,8 @@ public static class GameObjectExtensions
 	extension( GameObject gameObject )
 	{
 		/// <summary>
-		/// Clones and network spawns this GameObject.
+		/// Clones and network spawn this GameObject.
 		/// </summary>
-		/// <param name="gameObject">GameObject to spawn.</param>
 		/// <param name="owner">Connection to set as the owner.</param>
 		/// <param name="config">Clone configuration.</param>
 		/// <returns>Spawned GameObject</returns>
